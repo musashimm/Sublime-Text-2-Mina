23d430612bed41d54633968a9ac0ad23bf084c8e
Wojciech Todryk <wojciech@todryk.pl>
2013-01-17 18:30:14 +0100

    -a
    menu changed

--- mina.py
+++ mina.py
@@ -1,25 +1,73 @@
-import sublime, sublime_plugin, os
+import sublime, sublime_plugin, os, re
 
-class MinaDeployCommand(sublime_plugin.TextCommand):
+class MinaBase(sublime_plugin.TextCommand):
+  
+  def load_config(self):
+    s = sublime.load_settings("mina.sublime-settings")
+    global MINA_DEPLOY_CONFIG_DIR; MINA_DEPLOY_CONFIG_DIR = s.get("mina_deploy_config_dir")
+    global MINA_DEPLOY_CONFIG_FILE; MINA_DEPLOY_CONFIG_FILE = s.get("mina_deploy_config_file")
+
+  def find_project_dir(self):
+    fn = self.view.file_name()
+    l = fn.split(os.sep)
+    l.pop()
+    l.pop(0)
+    root = ""
+    while len(l):
+      pathname = os.sep+os.sep.join(l)
+      filename = pathname+os.sep+MINA_DEPLOY_CONFIG_DIR+os.sep+MINA_DEPLOY_CONFIG_FILE
+      #print pathname
+      if os.path.exists(filename):
+        root = pathname
+      #print l
+      l.pop()
+    return root
+
+  def run_shell_command(self, command, working_dir):
+    self.view.window().run_command("exec", {
+      "cmd": [command],
+      "shell": True,
+      "working_dir": working_dir
+    })
+
+  def error_desc(self,error_id):
+    errors = {'no_dir': "Can not find project directory for this file.\nCheck configuration and try again."}
+    if error_id in errors:
+      return "Mina: "+errors[error_id]
+    else:
+      return "Mina: "+"Unknown error."
+
+class MinaTask(MinaBase):
+  def run_task(self,task):
+    self.load_config()
+    project_root = self.find_project_dir()
+    if not project_root:
+      sublime.error_message(self.error_desc("no_dir"))
+      return
+
+    match = re.search("configuration", task)
+    if match:
+      filename = project_root+os.sep+MINA_DEPLOY_CONFIG_DIR+os.sep+MINA_DEPLOY_CONFIG_FILE
+      self.view.window().run_command("open_file",{"file":filename})
+    else:
+      self.run_shell_command(task,project_root)
+
+class MinaTasksCommand(MinaTask):
   def run(self, edit):
-    self.view.window().run_command("exec", { "cmd": 'mina deploy', "shell": True })
-      
+    self.run_task("mina tasks")
 
-class MinaTasksCommand(sublime_plugin.TextCommand):
+class MinaDeployCommand(MinaTask):
   def run(self, edit):
-    self.view.window().run_command("exec", { "cmd": 'mina tasks', "shell": True })
+    self.run_task("mina deploy")
 
-
-class MinaInitCommand(sublime_plugin.TextCommand):
+class MinaSetupCommand(MinaTask):
   def run(self, edit):
-    self.view.window().run_command("exec", { "cmd": 'mina init', "shell": True })
+    self.run_task("mina setup")
 
-
-class MinaSetupCommand(sublime_plugin.TextCommand):
+class MinaCleanupCommand(MinaTask):
   def run(self, edit):
-    self.view.window().run_command("exec", { "cmd": 'mina setup', "shell": True })
+    self.run_task("mina cleanup")
 
-
-class MinaCleanupCommand(sublime_plugin.TextCommand):
+class MinaConfigurationCommand(MinaTask):
   def run(self, edit):
-    self.view.window().run_command("exec", { "cmd": 'mina deploy:cleanup', "shell": True })
+    self.run_task("mina configuration")
